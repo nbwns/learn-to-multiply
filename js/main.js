f6d95743cf3eba1be80a95ce01044f14bf513610
nbwns <nicolas.bauwens@gmail.com>
2019-01-01 22:30:51 +0100

    initial

---
+++ js/main.js
@@ -0,0 +1,38 @@
+new Vue({
+    el: "#multiply",
+    data() {
+        return {
+            tables: [2,3,4,5,10],
+            selectedTable: 2,
+            answer: null,
+            random: 0,
+            randomMax: 10,
+            success: null
+        }
+    },
+    methods:{
+        newQuestion(){
+            this.success = null;
+            this.answer = null;
+            this.random = chance.integer({ min: 0, max: this.randomMax });
+        },
+        select(table){
+            this.selectedTable = table;
+            console.log(this.selectedTable);
+            this.newQuestion();
+        },
+        check(){
+            let verify = this.random * this.selectedTable;
+            console.log(verify, this.answer);
+            if(verify == this.answer){
+                this.success = true;
+            }
+            else{
+                this.success = false;
+            }
+        }
+    },
+    created(){
+        this.newQuestion();
+    }
+})
